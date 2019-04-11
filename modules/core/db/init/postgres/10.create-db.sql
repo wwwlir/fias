a3e63722692ccfe5b5ec9d6429f640fe13bc4866
sergeev_ms <sergeevms.33@gmail.com>
2019-04-11 11:43:14 +0300

    Missing postgres db-creation scripts.

--- modules/core/db/init/postgres/10.create-db.sql
+++ modules/core/db/init/postgres/10.create-db.sql
@@ -22,3 +22,33 @@ create table FIAS_FIAS_ENTITY (
     primary key (ID)
 )^
 -- end FIAS_FIAS_ENTITY
+-- begin FIAS_HOUSE
+create table FIAS_HOUSE (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    POSTALCODE varchar(6),
+    IFNSFL varchar(4),
+    TERRIFNSFL varchar(4),
+    IFNSUL varchar(4),
+    TERRIFNSUL varchar(4),
+    OKATO varchar(11),
+    OKTMO varchar(11),
+    HOUSENUM varchar(20),
+    ESTSTATUS integer,
+    BUILDNUM varchar(10),
+    STRUCNUM varchar(10),
+    STRSTATUS integer,
+    STARTDATE timestamp,
+    ENDDATE date,
+    PARENT_ID uuid not null,
+    --
+    primary key (ID)
+)^
+-- end FIAS_HOUSE
