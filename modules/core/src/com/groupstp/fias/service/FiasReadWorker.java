213b2d7847a1dbad05e871613fe0b420eda6da64
sergeev_ms <sergeevms.33@gmail.com>
2019-04-14 13:45:54 +0300

    Cleanup.

--- modules/core/src/com/groupstp/fias/service/FiasReadWorker.java
+++
@@ -1,9 +0,0 @@
-package com.groupstp.fias.service;
-
-import java.io.FileNotFoundException;
-
-public interface FiasReadWorker {
-    String NAME = "fias_FiasReadWorker";
-
-    public void readFias() throws FileNotFoundException;
-}
